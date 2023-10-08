6bbe0ec6eb18d83abfce621af5590dcf22288239
David <a20212557@pucp.edu.pe>
2023-10-07 22:55:08 -0500

    Aleluya 2.0

--- EstacionPesajeView/frmNuevoProyecto.h
+++ EstacionPesajeView/frmNuevoProyecto.h
@@ -48,11 +48,17 @@ namespace EstacionPesajeView {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"frmNuevoProyecto";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// frmNuevoProyecto
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(833, 438);
+			this->Name = L"frmNuevoProyecto";
+			this->Text = L"Nuevo Proyecto";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
